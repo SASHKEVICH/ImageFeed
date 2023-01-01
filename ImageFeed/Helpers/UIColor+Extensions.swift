1416828406955e0a264da2620b1b908fd23cfc21
Aleksandr Bekreneb <praise2001@icloud.com>
2023-01-01 23:31:23 +0700

    fix: profile vc with auto layout

---
+++ ImageFeed/Helpers/UIColor+Extensions.swift
@@ -0,0 +1,12 @@
+//
+//  UIColor+Extensions.swift
+//  ImageFeed
+//
+//  Created by Александр Бекренев on 01.01.2023.
+//
+
+import UIKit
+
+extension UIColor {
+    static let ypGray: UIColor = UIColor(named: "YP Gray") ?? UIColor()
+}
--- ImageFeed/Profile/ProfileViewController.swift
+++ ImageFeed/Profile/ProfileViewController.swift
@@ -8,20 +8,116 @@
 import UIKit
 
 class ProfileViewController: UIViewController {
-    @IBOutlet private weak var profileImage: UIImageView!
-    @IBOutlet private weak var nameLabel: UILabel!
-    @IBOutlet private weak var nicknameLabel: UILabel!
-    @IBOutlet private weak var textLabel: UILabel!
-    @IBOutlet private weak var exitButton: UIButton!
+    let stackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.alignment = .leading
+        stackView.spacing = 8
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
+    }()
+    
+    let profileImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(systemName: "person.crop.circle.fill")
+        imageView.backgroundColor = .white
+        imageView.clipsToBounds = true
+        imageView.tintColor = .gray
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    let nameLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .white
+        label.font = UIFont.boldSystemFont(ofSize: 23)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    let nicknameLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .ypGray
+        label.font = UIFont.systemFont(ofSize: 13)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    let textLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .white
+        label.font = UIFont.systemFont(ofSize: 13)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    let exitButton: UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(systemName: "ipad.and.arrow.forward") ?? UIImage(), for: .normal)
+        button.tintColor = .red
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        setupProfileImage()
+        layoutStackView()
+        layoutExitButton()
     }
     
-    private func setupProfileImage() {
-        profileImage.clipsToBounds = true
-        profileImage.layer.cornerRadius = profileImage.bounds.height / 2
+    private func layoutStackView() {
+        view.addSubview(stackView)
+        stackView.addArrangedSubview(profileImageView)
+        stackView.addArrangedSubview(nameLabel)
+        stackView.addArrangedSubview(nicknameLabel)
+        stackView.addArrangedSubview(textLabel)
+        
+        stackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20).isActive = true
+        stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20).isActive = true
+        stackView.widthAnchor.constraint(greaterThanOrEqualToConstant: 235).isActive = true
+        
+        layoutProfileImageView()
+        layoutNameLabel()
+        layoutNicknameLabel()
+        layoutTextLabel()
+    }
+    
+    private func layoutProfileImageView() {
+        let widthAndHeight: CGFloat = 70
+        
+        let constraints = [
+            profileImageView.leadingAnchor.constraint(equalTo: stackView.leadingAnchor),
+            profileImageView.widthAnchor.constraint(equalToConstant: widthAndHeight),
+            profileImageView.heightAnchor.constraint(equalToConstant: widthAndHeight),
+        ]
+        NSLayoutConstraint.activate(constraints)
+        profileImageView.layer.cornerRadius = widthAndHeight / 2
+    }
+    
+    private func layoutNameLabel() {
+        nameLabel.text = "Александр Бекренев"
+    }
+    
+    private func layoutNicknameLabel() {
+        nicknameLabel.text = "@sashkevich"
+    }
+    
+    private func layoutTextLabel() {
+        textLabel.text = "Hello, World!"
+    }
+    
+    private func layoutExitButton() {
+        view.addSubview(exitButton)
+        exitButton.contentHorizontalAlignment = .fill
+        exitButton.contentVerticalAlignment = .fill
+        
+        let constraints = [
+            exitButton.centerYAnchor.constraint(equalTo: profileImageView.centerYAnchor),
+            exitButton.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -20),
+            exitButton.widthAnchor.constraint(equalToConstant: 24),
+            exitButton.heightAnchor.constraint(equalToConstant: 24),
+        ]
+        NSLayoutConstraint.activate(constraints)
     }
 }
