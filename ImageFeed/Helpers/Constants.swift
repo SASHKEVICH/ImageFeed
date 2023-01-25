535a16c9dc07fcefb75e536e08bb3a61fc83238d
Aleksandr Bekrenev <praise2001@yandex.ru>
2023-01-25 17:08:53 +0700

    env: unsplash env constants

---
+++ ImageFeed/Helpers/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  ImageFeed
+//
+//  Created by Александр Бекренев on 22.01.2023.
+//
+
+import Foundation
+
+let AccessKey = "eKVDZrFSvICDbGxnM_8dJnQ_0Smh9Xxxehq3jS5seds"
+let SecretKey = "rwJzVgnKH_TL4hEYsThbZlBzwG83uTY1FHS0CQPhTck"
+let RedirectURI = "urn:ietf:wg:oauth:2.0:oob"
+
+let AccessScopes = "public+read_user+write_likes"
+let DefaultBaseURL = URL(string: "https://api.unsplash.com")!
