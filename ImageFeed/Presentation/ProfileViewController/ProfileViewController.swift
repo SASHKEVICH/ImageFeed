07da18423ff771cee9183401d7d1aae31695b627
Aleksandr Bekrenev <praise2001@yandex.ru>
2023-02-10 12:25:23 +0700

    feat: extract profile view controller from storyboard

--- ImageFeed/Presentation/ProfileViewController/ProfileViewController.swift
+++ ImageFeed/Presentation/ProfileViewController/ProfileViewController.swift
@@ -66,6 +66,7 @@ final class ProfileViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .ypBlack
         
         layoutStackView()
         layoutExitButton()
