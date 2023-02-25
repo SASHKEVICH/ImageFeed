451e2a89a5093b14dd3fe17e8d4296e2b131049d
Aleksandr Bekrenev <praise2001@yandex.ru>
2023-02-25 17:22:12 +0700

    fix: "profile" under profile icon in tab bar item

--- ImageFeed/Presentation/TabBarController.swift
+++ ImageFeed/Presentation/TabBarController.swift
@@ -19,7 +19,7 @@ final class TabBarController: UITabBarController {
         
         let profileViewController = ProfileViewController()
         profileViewController.tabBarItem = UITabBarItem(
-            title: NSLocalizedString("Profile", comment: ""),
+            title: "",
             image: UIImage(named: "tab_profile_active"),
             selectedImage: nil
         )
