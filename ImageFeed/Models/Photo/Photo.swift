526f8f4d8aed9df0e3a3692cf21d605b71f20362
Aleksandr Bekrenev <praise2001@yandex.ru>
2023-03-03 17:24:41 +0700

    refactor: extract photo struct to its file

---
+++ ImageFeed/Models/Photo/Photo.swift
@@ -0,0 +1,46 @@
+//
+//  Photo.swift
+//  ImageFeed
+//
+//  Created by Александр Бекренев on 03.03.2023.
+//
+
+import Foundation
+
+struct Photo {
+    let id: String
+    let size: CGSize
+    let createdAt: Date?
+    let welcomeDescription: String?
+    let thumbImageURL: String
+    let largeImageURL: String
+    let isLiked: Bool
+    
+    init(from result: PhotoResult) {
+        self.id = result.id
+        self.size = CGSize(width: result.width, height: result.height)
+        self.createdAt = result.createdAt
+        self.welcomeDescription = result.description
+        self.thumbImageURL = result.urls.thumb
+        self.largeImageURL = result.urls.full
+        self.isLiked = result.likedByUser
+    }
+    
+    init(
+        id: String,
+        size: CGSize,
+        createdAt: Date?,
+        welcomeDescription: String?,
+        thumbImageURL: String,
+        largeImageURL: String,
+        isLiked: Bool
+    ) {
+        self.id = id
+        self.size = size
+        self.createdAt = createdAt
+        self.welcomeDescription = welcomeDescription
+        self.thumbImageURL = thumbImageURL
+        self.largeImageURL = largeImageURL
+        self.isLiked = isLiked
+    }
+}
--- ImageFeed/Services/ImagesList/ImagesListService.swift
+++ ImageFeed/Services/ImagesList/ImagesListService.swift
@@ -9,44 +9,6 @@ import Foundation
 
 final class ImagesListService {
     
-    struct Photo {
-        let id: String
-        let size: CGSize
-        let createdAt: Date?
-        let welcomeDescription: String?
-        let thumbImageURL: String
-        let largeImageURL: String
-        let isLiked: Bool
-        
-        init(from result: PhotoResult) {
-            self.id = result.id
-            self.size = CGSize(width: result.width, height: result.height)
-            self.createdAt = result.createdAt
-            self.welcomeDescription = result.description
-            self.thumbImageURL = result.urls.thumb
-            self.largeImageURL = result.urls.full
-            self.isLiked = result.likedByUser
-        }
-        
-        init(
-            id: String,
-            size: CGSize,
-            createdAt: Date?,
-            welcomeDescription: String?,
-            thumbImageURL: String,
-            largeImageURL: String,
-            isLiked: Bool
-        ) {
-            self.id = id
-            self.size = size
-            self.createdAt = createdAt
-            self.welcomeDescription = welcomeDescription
-            self.thumbImageURL = thumbImageURL
-            self.largeImageURL = largeImageURL
-            self.isLiked = isLiked
-        }
-    }
-    
     static let didChangeNotification = Notification.Name(rawValue: "ImagesListServiceDidChange")
     
     private let urlSession = URLSession.shared
