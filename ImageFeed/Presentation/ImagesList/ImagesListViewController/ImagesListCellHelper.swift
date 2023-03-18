bdd64c6f7d741d150d49f62070f267a7d37b4c6a
Aleksandr Bekrenev <praise2001@yandex.ru>
2023-03-18 17:25:08 +0700

    feat: using images list helper with test env

--- ImageFeed/Presentation/ImagesList/ImagesListViewController/ImagesListCellHelper.swift
+++ ImageFeed/Presentation/ImagesList/ImagesListViewController/ImagesListCellHelper.swift
@@ -20,9 +20,22 @@ public protocol ImagesListCellHelperProtocol {
         tableViewWidth: CGFloat,
         for: Photo
     ) -> CGFloat
+    func setCellLoadingStateIfItsImageNil(_: UITableViewCell)
 }
 
+public enum RetrievingImageConfiguration {
+    case test(UIImage)
+    case production
+}
+
+// MARK: - Interface methods
 struct ImagesListCellHelper: ImagesListCellHelperProtocol {
+    private let retrievingImageConfiguration: RetrievingImageConfiguration
+    
+    init(configuration: RetrievingImageConfiguration = .production) {
+        retrievingImageConfiguration = configuration
+    }
+    
     func configured(
         cell: ImagesListCell,
         at indexPath: IndexPath,
@@ -46,8 +59,26 @@ struct ImagesListCellHelper: ImagesListCellHelperProtocol {
         let cellHeight = photo.size.height * scale + imageInsets.top + imageInsets.bottom
         return cellHeight
     }
+    
+    func setCellLoadingStateIfItsImageNil(_ cell: UITableViewCell) {
+        if let cell = cell as? ImagesListCell, cell.cellImage == nil {
+            cell.cellState = .loading
+        }
+    }
 }
 
+// MARK: - Internal helper methods
+extension ImagesListCellHelper {
+    func set(image: UIImage, for cell: ImagesListCell) {
+        cell.cellState = .finished(image)
+    }
+    
+    func setErrorState(for cell: ImagesListCell) {
+        cell.cellState = .error
+    }
+}
+
+// MARK: - Private helper methods
 private extension ImagesListCellHelper {
     func configure(
         cell: ImagesListCell,
@@ -55,16 +86,13 @@ private extension ImagesListCellHelper {
         with photo: Photo,
         completion: @escaping () -> Void
     ) -> ImagesListCell {
-        if let imageURL = URL(string: photo.thumbImageURL) {
-            setImageFor(cell: cell, with: imageURL, completion: completion)
-        }
+        configureImage(for: cell, with: photo, completion: completion)
         cell.cellDateText = photo.createdAt.imagesListCellDateString()
         cell.isLiked = photo.isLiked
-        
         return cell
     }
     
-    func setImageFor(
+    func downloadImageFor(
         cell: ImagesListCell,
         with imageURL: URL,
         completion rowsReloadingCompletion: @escaping () -> Void
@@ -72,12 +100,27 @@ private extension ImagesListCellHelper {
         KingfisherManager.shared.retrieveImage(with: imageURL) { result in
             switch result {
             case .success(let imageResult):
-                cell.cellState = .finished(imageResult.image)
+                set(image: imageResult.image, for: cell)
             case .failure(_):
-                cell.cellState = .error
+                setErrorState(for: cell)
             }
 
             rowsReloadingCompletion()
+        }
+    }
+    
+    func configureImage(
+        for cell:
+        ImagesListCell,
+        with photo: Photo,
+        completion: @escaping () -> Void
+    ) {
+        switch retrievingImageConfiguration {
+        case .production:
+            guard let imageURL = URL(string: photo.thumbImageURL) else { return }
+            downloadImageFor(cell: cell, with: imageURL, completion: completion)
+        case .test(let image):
+            set(image: image, for: cell)
         }
     }
 }
