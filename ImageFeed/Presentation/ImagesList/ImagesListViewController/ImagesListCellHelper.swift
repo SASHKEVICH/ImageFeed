c9eb9fce9a0910ce64ed5f07ace0e119e8f79607
Aleksandr Bekrenev <praise2001@yandex.ru>
2023-03-18 20:56:24 +0700

    refactor: extracting downloading images for cell logic

--- ImageFeed/Presentation/ImagesList/ImagesListViewController/ImagesListCellHelper.swift
+++ ImageFeed/Presentation/ImagesList/ImagesListViewController/ImagesListCellHelper.swift
@@ -6,49 +6,49 @@
 //
 
 import UIKit
-import Kingfisher
 
-public protocol ImagesListCellHelperProtocol {
+public protocol ImagesListCellHelperDelegate: AnyObject {
+    func didSet(image: UIImage, for cell: ImagesListCell)
+    func didSetErrorState(for cell: ImagesListCell)
+}
+
+public protocol ImagesListCellHelperProtocol: AnyObject, ImagesListCellHelperDelegate {
+    var imageLoader: ImagesListCellImageLoaderProtocol? { get }
     func configured(
         cell: ImagesListCell,
-        at: IndexPath,
         with: Photo,
         completion: @escaping () -> Void
     ) -> ImagesListCell
     func calculateCellHeight(
-        at: IndexPath,
         tableViewWidth: CGFloat,
         for: Photo
     ) -> CGFloat
     func setCellLoadingStateIfItsImageNil(_: UITableViewCell)
 }
 
-public enum RetrievingImageConfiguration {
-    case test(UIImage)
-    case production
-}
-
 // MARK: - Interface methods
-struct ImagesListCellHelper: ImagesListCellHelperProtocol {
-    private let retrievingImageConfiguration: RetrievingImageConfiguration
+final class ImagesListCellHelper: ImagesListCellHelperProtocol {
+    var imageLoader: ImagesListCellImageLoaderProtocol?
     
-    init(configuration: RetrievingImageConfiguration = .production) {
-        retrievingImageConfiguration = configuration
+    init(imageLoader: ImagesListCellImageLoaderProtocol = ImagesListCellImageLoader()) {
+        self.imageLoader = imageLoader
+        imageLoader.helper = self
     }
     
     func configured(
         cell: ImagesListCell,
-        at indexPath: IndexPath,
         with photo: Photo,
         completion: @escaping () -> Void
     ) -> ImagesListCell {
-        let configuredCell = configure(cell: cell, at: indexPath, with: photo, completion: completion)
+        let configuredCell = configure(
+            cell: cell,
+            with: photo,
+            completion: completion)
         configuredCell.selectionStyle = .none
         return configuredCell
     }
     
     func calculateCellHeight(
-        at indexPath: IndexPath,
         tableViewWidth: CGFloat,
         for photo: Photo
     ) -> CGFloat {
@@ -67,13 +67,13 @@ struct ImagesListCellHelper: ImagesListCellHelperProtocol {
     }
 }
 
-// MARK: - Internal helper methods
-extension ImagesListCellHelper {
-    func set(image: UIImage, for cell: ImagesListCell) {
+// MARK: - ImagesListCellHelperDelegate
+extension ImagesListCellHelper: ImagesListCellHelperDelegate {
+    func didSet(image: UIImage, for cell: ImagesListCell) {
         cell.cellState = .finished(image)
     }
     
-    func setErrorState(for cell: ImagesListCell) {
+    func didSetErrorState(for cell: ImagesListCell) {
         cell.cellState = .error
     }
 }
@@ -82,45 +82,13 @@ extension ImagesListCellHelper {
 private extension ImagesListCellHelper {
     func configure(
         cell: ImagesListCell,
-        at indexPath: IndexPath,
         with photo: Photo,
         completion: @escaping () -> Void
     ) -> ImagesListCell {
-        configureImage(for: cell, with: photo, completion: completion)
+        imageLoader?.requestLoadImage(for: cell, with: photo, completion: completion)
+        
         cell.cellDateText = photo.createdAt.imagesListCellDateString()
         cell.isLiked = photo.isLiked
         return cell
-    }
-    
-    func downloadImageFor(
-        cell: ImagesListCell,
-        with imageURL: URL,
-        completion rowsReloadingCompletion: @escaping () -> Void
-    ) {
-        KingfisherManager.shared.retrieveImage(with: imageURL) { result in
-            switch result {
-            case .success(let imageResult):
-                set(image: imageResult.image, for: cell)
-            case .failure(_):
-                setErrorState(for: cell)
-            }
-
-            rowsReloadingCompletion()
-        }
-    }
-    
-    func configureImage(
-        for cell:
-        ImagesListCell,
-        with photo: Photo,
-        completion: @escaping () -> Void
-    ) {
-        switch retrievingImageConfiguration {
-        case .production:
-            guard let imageURL = URL(string: photo.thumbImageURL) else { return }
-            downloadImageFor(cell: cell, with: imageURL, completion: completion)
-        case .test(let image):
-            set(image: image, for: cell)
-        }
     }
 }
